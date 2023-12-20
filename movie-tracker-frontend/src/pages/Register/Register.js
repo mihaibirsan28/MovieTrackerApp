df1d1d5762b1093576cdfbffd5b2cc451b64f602
RobertLita <lita.robert@yahoo.com>
2023-12-20 22:29:57 +0200

    redirect to login

--- movie-tracker-frontend/src/pages/Register/Register.js
+++ movie-tracker-frontend/src/pages/Register/Register.js
@@ -1,121 +1,135 @@
-import React, { useState } from 'react';
-import { TextField, Button, Container, Grid, Typography } from '@mui/material';
-import { properties } from '../../properties';
-import './Register.css'
-import axios from 'axios';
+import React, { useState } from "react";
+import { TextField, Button, Container, Grid, Typography } from "@mui/material";
+import { properties } from "../../properties";
+import "./Register.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
-    const [formData, setFormData] = useState({
-        first_name: '',
-        last_name: '',
-        email: '',
-        username: '',
-        password: '',
-        confirm_password: ''
-      });
-    
-      const handleChange = (e) => {
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-        });
-      };
-    
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        try {
-          const response = await axios.post(properties.BACKEND_HOST + "/register", formData);
-          console.log('Response from the server:', response.data);
-        } catch (error) {
-          
-        }
-      };
-    
-      return (
-        <Container id="grid" component="main" maxWidth="md">
-          <div>
-            <Typography id="title" align='center' variant='h3'>Register</Typography>
-            <form onSubmit={handleSubmit}>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    label="First Name"
-                    name="first_name"
-                    value={formData.first_name}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    label="Last Name"
-                    name="last_name"
-                    value={formData.last_name}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    label="Email Address"
-                    name="email"
-                    type="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    label="Username"
-                    name="username"
-                    type="username"
-                    value={formData.username}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    label="Password"
-                    name="password"
-                    type="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    label="Confirm Password"
-                    name="confirm_password"
-                    type="password"
-                    value={formData.confirm_password}
-                    onChange={handleChange}
-                  />
-                </Grid>
-              </Grid>
-              <Button id="button" type="submit" fullWidth variant="contained" color="warning">
-                Register
-              </Button>
-            </form>
-          </div>
-        </Container>
+  let navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    username: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        properties.BACKEND_HOST + "/register",
+        formData
       );
+      if (response.status == 201) {
+        return navigate("/login");
+      }
+      console.log("Response from the server:", response.data);
+    } catch (error) {}
+  };
+
+  return (
+    <Container id="grid" component="main" maxWidth="md">
+      <div>
+        <Typography id="title" align="center" variant="h3">
+          Register
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="First Name"
+                name="first_name"
+                value={formData.first_name}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Last Name"
+                name="last_name"
+                value={formData.last_name}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Email Address"
+                name="email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Username"
+                name="username"
+                type="username"
+                value={formData.username}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Password"
+                name="password"
+                type="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Confirm Password"
+                name="confirm_password"
+                type="password"
+                value={formData.confirm_password}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            id="button"
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="warning"
+          >
+            Register
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
 }
 
 export default Register;
